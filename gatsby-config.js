53215161188dac80643d2b1737a5d93dd2f14659
Anton Dosov <dosantappdev@gmail.com>
2021-03-13 21:04:08 +0100

    gatsby-plugin-canonical-urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,7 +115,14 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site. // TODO
       },
     },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://adosov.dev/`,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-gatsby-cloud`,
   ],
+
 }
